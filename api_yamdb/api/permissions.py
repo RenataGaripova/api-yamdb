ead8ce8d94b204fb17377697cc3f5a7e32bddbf6
Renata Garipova <rena.garipova.2005@gmail.com>
2025-09-11 23:14:09 +0500

    final commit

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -7,21 +7,6 @@ class IsAdmin(permissions.BasePermission):
         return request.user.is_authenticated and request.user.is_admin
 
 
-class IsModerator(permissions.BasePermission):
-    """Permission, проверяющий, что пользователь - модератор."""
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.is_moderator
-
-
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    """Permission, разрешающий запись только владельцу."""
-    def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or obj.author == request.user
-        )
-
-
 class IsAdminOrReadOnly(permissions.BasePermission):
     """Permission, разрешающий запись и изменение только админу."""
     def has_permission(self, request, view):
